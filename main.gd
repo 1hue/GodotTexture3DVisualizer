8c73f843da8200e34c6dc8875d375a8178ef1ac3
0x1hue <212147609+0x1hue@users.noreply.github.com>
2025-09-17 12:25:09 +0100

    Add perlin noise texture for demo

--- main.gd
+++ main.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+const CellularNoise: NoiseTexture3D = preload("res://resources/cellular_noise_texture_3d.tres")
+const PerlinNoise: NoiseTexture3D = preload("res://resources/perlin_noise_texture_3d.tres")
+
+
 @onready var camera_pivot: Node3D = %CameraPivot
 
 var cam_rotation: float = deg_to_rad(30)
