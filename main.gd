d912f3f677dce37ddcf67591150d86c953ec51d3
0x1hue <212147609+0x1hue@users.noreply.github.com>
2025-09-17 14:32:25 +0100

    Fix note

--- main.gd
+++ main.gd
@@ -5,8 +5,17 @@ const PerlinNoise: NoiseTexture3D = preload("res://resources/perlin_noise_textur
 
 
 @onready var camera_pivot: Node3D = %CameraPivot
+@onready var node: MeshInstance3D = $Cube
 
 var cam_rotation: float = deg_to_rad(30)
 
 func _process(delta: float) -> void:
 	camera_pivot.rotate_y(cam_rotation * delta)
+
+func set_texture(texture: Texture3D) -> void:
+	var mesh: BoxMesh = node.mesh
+	# Grab the material. This depends on how/where you set it.
+	var material: ShaderMaterial = mesh.material
+	#var material: ShaderMaterial = mesh.surface_get_material(0)
+	#var material: ShaderMaterial = node.get_surface_override_material(0)
+	material.set_shader_parameter(&"tex", texture)
